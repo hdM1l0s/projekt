5b58844510ca210df28f80b5bf3e4accbce8b6b4
hdM1l0s <u63764@student.wat.edu.pl>
2025-06-24 10:46:51 +0200

    wip: preparet menu

--- main.py
+++ main.py
@@ -27,3 +27,25 @@ class Client:
         self.archive = archive
         self.coordinates = get_coordinates(location)
         self.marker = map_widget.set_marker(*self.coordinates, text=name)
+def get_coordinates(location):
+    import requests
+    from bs4 import BeautifulSoup
+    import re
+
+    def dms_to_dd(dms_str):
+        parts = re.split('[°′″]', dms_str)
+        degrees = float(parts[0])
+        minutes = float(parts[1])
+        seconds = float(parts[2])
+        direction = dms_str[-1]
+        dd = degrees + minutes / 60 + seconds / 3600
+        if direction in ['S', 'W']:
+            dd *= -1
+        return dd
+
+    url = f'https://pl.wikipedia.org/wiki/{location}'
+    response = requests.get(url).text
+    soup = BeautifulSoup(response, 'html.parser')
+    lat_str = soup.select_one('.latitude').text
+    lon_str = soup.select_one('.longitude').text
+    return dms_to_dd(lat_str), dms_to_dd(lon_str)
